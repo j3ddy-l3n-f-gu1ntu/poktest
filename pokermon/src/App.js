babb0f87d5b778a14da708d6ca666c3fc61da950
Jed Guintu <41206975+j3ddy-l3n-f-gu1ntu@users.noreply.github.com>
2020-08-19 19:41:28 -0700

    added loading screen so site doesn't look frozen, and useState's

--- pokermon/src/App.js
+++ pokermon/src/App.js
@@ -1,8 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokeList from './PokeList';
+import axios from 'axios'
 
 function App() {
-  const [pokemon, setPokemon] = useState(["ditto", "mewtwo"]);
+  const [pokemon, setPokemon] = useState([]);
+  const [currentURL, setCurrURL] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextURL, setNextURL] = useState();
+  const [prevURL, setPrevURL] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel
+    axios.get(currentURL, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false);
+      setNextURL(res.data.next)
+      setPrevURL(res.data.prev)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => {
+      cancel()
+    }
+
+  }, [currentURL])
+
+
+  function goNextPage () {
+    
+  }
+
+
+  if (loading) return `Loading...`
+
   const user = {
     firstName: 'joe',
     lastName: 'mama'
