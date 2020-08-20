678e2e4872a3565d7dc4d0fcee5c32ce52053521
Jed Guintu <41206975+j3ddy-l3n-f-gu1ntu@users.noreply.github.com>
2020-08-19 19:20:49 -0700

    installed axios tested out stuff

--- pokermon/src/App.js
+++ pokermon/src/App.js
@@ -3,6 +3,10 @@ import PokeList from './PokeList';
 
 function App() {
   const [pokemon, setPokemon] = useState(["ditto", "mewtwo"]);
+  const user = {
+    firstName: 'joe',
+    lastName: 'mama'
+  };
   return (
     <PokeList pokemon={pokemon} />
   );
